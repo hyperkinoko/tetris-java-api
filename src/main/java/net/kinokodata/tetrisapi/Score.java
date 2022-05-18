3970c9123a0bade42519ea4fafee99bfaee2b852
hyperkinoko <superkinoko@icloud.com>
2022-05-19 05:18:36 +0900

    修正チャレンジ

--- src/main/java/net/kinokodata/tetrisapi/Score.java
+++ src/main/java/net/kinokodata/tetrisapi/Score.java
@@ -14,28 +14,4 @@ public class Score {
     private int id;
     private int score;
     private String name;
-    
-    public int getScore() {
-        return score;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/net/kinokodata/tetrisapi/ScoreController.java
+++ src/main/java/net/kinokodata/tetrisapi/ScoreController.java
@@ -1,36 +1,8 @@
 package net.kinokodata.tetrisapi;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class ScoreController {
     @Autowired
     private ScoreRepository scoreRepository;
     
-    @GetMapping("/scores")
-    public Iterable<Score> doGetScore(@RequestParam("score") int scoreValue, @RequestParam String name) {
-        Score score = new Score();
-        score.setScore(scoreValue);
-        score.setName(name);
-        scoreRepository.save(score);
-        return scoreRepository.findFirst10ByOrderByScoreDesc();
-    }
-
-    @PostMapping("/scores")
-    public Score doPostScore(@RequestBody ScorePostRequest requestBody) {
-        Score score = new Score();
-        score.setScore(requestBody.getScore());
-        score.setName(requestBody.getName());
-        scoreRepository.save(score);
-        return score;
-    }
 }
